04d866958095af3e1ccd790ff749834e7171d333
arora <aroramadhav65@gmail.com>
2022-05-13 13:59:01 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animated',
+      title: 'Animated Error',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

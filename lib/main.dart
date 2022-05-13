9f644f91b4836a4e9630cf9b68b28664514cedcd
arora <aroramadhav65@gmail.com>
2022-05-13 14:09:53 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Animated Error',
+      title: 'Animated Error Page',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),

d2d08cc799813d798999600c651f44a889d93bc0
arora <aroramadhav65@gmail.com>
2022-05-13 14:06:46 +0530

    Update

--- lib/screens/error/components/body.dart
+++ lib/screens/error/components/body.dart
@@ -6,6 +6,8 @@ import 'package:flutter_svg/svg.dart';
 import 'error_text.dart';
 
 class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
   @override
   _BodyState createState() => _BodyState();
 }

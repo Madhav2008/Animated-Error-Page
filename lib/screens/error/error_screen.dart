1c02b569def33f030ff13b6a76fe9aed5f1d09ce
arora <aroramadhav65@gmail.com>
2022-05-13 14:08:10 +0530

    Update

--- lib/screens/error/error_screen.dart
+++ lib/screens/error/error_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'components/body.dart';
 
 class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
